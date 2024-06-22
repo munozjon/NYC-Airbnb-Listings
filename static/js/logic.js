8a959ce6072868fc6058b57d85216808a44b740c
Zainab Shafi <zainabfshafi95@gmail.com>
2024-06-22 14:42:06 -0400

    summary stats info for private airbnb

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,18 +6,18 @@ function buildCharts(borough) {
 
         // Filter the data for the desired borough
         let filteredData = parsedData.filter(results => results.Borough == borough);
+        console.log(filteredData)
 
-
-        // Create the pie chart
+        //Create the pie chart
 
         // Calculate the total number of rooms per room type
         function calculateRoomTypeCounts(data) {
             const counts = {};
-        
+
             data.forEach(document => {
                 const roomType = document["Room Type"];
                 const count = document["Room Count"];
-            
+
                 if (counts[roomType]) {
                     counts[roomType] += count;
                 }
@@ -25,10 +25,10 @@ function buildCharts(borough) {
                     counts[roomType] = count;
                 }
             });
-          
+
             return counts;
         };
-          
+
         const roomTypeCounts = calculateRoomTypeCounts(filteredData);
 
         // Initialize the data for the chart
@@ -43,87 +43,114 @@ function buildCharts(borough) {
 
         var layout = {
             title: "% of rooms per Room Type",
-            margin: {"t": 50, "b": 0, "l": 0, "r": 0}
-          };
+            margin: { "t": 50, "b": 0, "l": 0, "r": 0 }
+        };
 
-        
-        var config = {responsive: true};
+
+        var config = { responsive: true };
 
         Plotly.newPlot('pie', trace1, layout, config);
 
-              // Retrieve chart data
-              let boxPlotData = filteredData.reduce((name, item) => {
-                if (!name[item["Room Type"]]) {
-                    name[item["Room Type"]] = [];
-                }
-                name[item["Room Type"]].push(item["Average Price"]);
-                return name
-            }, {})
-    
-            console.log(boxPlotData)
-    
-    
-            // Initiate chart data
-    
-            let entireHomeTrace = {
-                y: boxPlotData["Entire home/apt"],
-                type: "box",
-                name: "Entire Home"
-    
+        // Retrieve chart data
+        let boxPlotData = filteredData.reduce((name, item) => {
+            if (!name[item["Room Type"]]) {
+                name[item["Room Type"]] = [];
             }
-            let hotelRoomTrace = {
-                y: boxPlotData["Hotel room"],
-                type: "box",
-                name: "Hotel Room"
-            }
-            let privateRoomTrace = {
-                y: boxPlotData["Private room"],
-                type: "box",
-                name: "Private Room"
-            }
-            let sharedRoomTrace = {
-                y: boxPlotData["Shared room"],
-                type: "box",
-                name: "Shared Room"
-            }
-            let all_data = [entireHomeTrace, hotelRoomTrace, privateRoomTrace, sharedRoomTrace]
-    
-            const priceLayout = {
-                title: "Average Prices by Borough"
-            };
+            name[item["Room Type"]].push(item["Average Price"]);
+            return name
+        }, {})
 
-                    Plotly.newPlot('box', all_data, priceLayout);
-    });
-};
+        console.log(boxPlotData)
+
+
+        // Initiate chart data
+
+        let entireHomeTrace = {
+            y: boxPlotData["Entire home/apt"],
+            type: "box",
+            name: "Entire Home"
+
+        }
+        let hotelRoomTrace = {
+            y: boxPlotData["Hotel room"],
+            type: "box",
+            name: "Hotel Room"
+        }
+        let privateRoomTrace = {
+            y: boxPlotData["Private room"],
+            type: "box",
+            name: "Private Room"
+        }
+        let sharedRoomTrace = {
+            y: boxPlotData["Shared room"],
+            type: "box",
+            name: "Shared Room"
+        }
+        let all_data = [entireHomeTrace, hotelRoomTrace, privateRoomTrace, sharedRoomTrace]
+
+        const priceLayout = {
+            title: "Average Prices by Borough"
+        };
+
+        Plotly.newPlot('box', all_data, priceLayout);
+
+
+        //Get the summary stats: 
+
+        let entire_home = filteredData.filter(item => item["Room Type"] == "Entire home/apt");
+
+        let maxPrice = d3.max(entire_home, function (d) {
+            return d["Average Price"];
+        });
+
+        let minPrice = d3.min(entire_home, function (d) {
+            return d["Average Price"];
+        });
+
+        let medPrice = d3.median(entire_home, function (d) {
+            return d["Average Price"];
+        });
+
+        let statsTab = d3.select("#stats");
+        statsTab.html("");
+        statsTab.append("h5").text(`Max Price: ${maxPrice.toFixed(2)}`)
+        statsTab.append("h5").text(`Med Price: ${medPrice.toFixed(2)}`);
+        statsTab.append("h5").text(`Min Price: ${minPrice.toFixed(2)}`);
+
+    
+;
+
+
+ })}
 
 // Create bar graph
-function buildBar(borough){ 
+function buildBar(borough){
     d3.json("http://127.0.0.1:5000/amenities").then((data) => {
-    
+
         let parsedAmData = JSON.parse(data);
 
         //Filter data for chosen borough:
-        let filteredAmData = parsedAmData.filter(results => results.Borough == borough); 
-        
+        let filteredAmData = parsedAmData.filter(results => results.Borough == borough);
+
         const neighbourhoodCounts = {};
-        
+
         filteredAmData.forEach(document => {
-                const neighbourhood = document["Neighbourhood"];
-                const amenity = document["amenities"];
-                const count = document["count"];
-                //
-                if (!neighbourhoodCounts[neighbourhood]) {
-                    neighbourhoodCounts[neighbourhood] = {};
-                }
-                if (neighbourhoodCounts[neighbourhood][amenity]){
-                    neighbourhoodCounts[neighbourhood][amenity] += count;
-                } else {
-                    neighbourhoodCounts[neighbourhood][amenity] = count;
-                }
-                }
-             );
-          
-            
+            const neighbourhood = document["Neighbourhood"];
+            const amenity = document["amenities"];
+            const count = document["count"];
+            //
+            if (!neighbourhoodCounts[neighbourhood]) {
+                neighbourhoodCounts[neighbourhood] = {};
+            }
+            if (neighbourhoodCounts[neighbourhood][amenity]) {
+                neighbourhoodCounts[neighbourhood][amenity] += count;
+            } else {
+                neighbourhoodCounts[neighbourhood][amenity] = count;
+            }
+        }
+        );
+
+
         const neighbourhoods = Object.keys(neighbourhoodCounts)
 
         //setting up an array to iterate and create traces for each amenity
@@ -137,22 +164,25 @@ function buildBar(borough){
                 y: neighbourhoods.map(neighbourhood => neighbourhoodCounts[neighbourhood][amenity] || 0),
                 name: amenity,
                 type: 'bar'
-            };  
+            };
         });
 
-        const layout = { 
-            title: "Popular Neighbourhood Amenities", 
+        const layout = {
+            title: "Popular Neighbourhood Amenities",
             barmode: 'stack',
-            yaxis: {title: 'Count'}
+            yaxis: { title: 'Count' }
         };
 
-        const barConfig = {responsive: true};
-
+        const barConfig = { responsive: true };
 
         Plotly.newPlot('bar', traces, layout, barConfig);
 
-})};
 
+    })
+    ;
+
+
+}
 // Initialize the map
 mapboxgl.accessToken = 'pk.eyJ1IjoiY21kdXJhbiIsImEiOiJjbHgxM2l1YTEwMjYxMmxwcnFoM2pkYnp0In0.HTQaiKsTNpDofxvQJwjvXg';
 const map = new mapboxgl.Map({
@@ -173,7 +203,7 @@ map.on('style.load', () => {
 
     map.addSource('neighborhoods', {
         type: 'geojson',
-        data: "https://data.insideairbnb.com/united-states/ny/new-york-city/2023-12-04/visualisations/neighbourhoods.geojson" 
+        data: "https://data.insideairbnb.com/united-states/ny/new-york-city/2023-12-04/visualisations/neighbourhoods.geojson"
     });
 
     //colors for each neighbourhood_group
@@ -183,7 +213,7 @@ map.on('style.load', () => {
         'Manhattan': '#0000ff',    // Blue for Manhattan
         'Queens': '#ffff00',       // Yellow for Queens
         'Staten Island': '#ff00ff' // Purple for Staten Island
-    };     
+    };
 
     // Add a layer to display the neighborhoods
     map.addLayer({
@@ -193,7 +223,7 @@ map.on('style.load', () => {
         'layout': {},
         'paint': {
             'fill-color': [
-                'match', ['get', 'neighbourhood_group'], 
+                'match', ['get', 'neighbourhood_group'],
                 'Bronx', colors['Bronx'],       // Color for Bronx
                 'Brooklyn', colors['Brooklyn'], // Color for Brooklyn
                 'Manhattan', colors['Manhattan'], // Color for Manhattan
@@ -212,21 +242,21 @@ map.on('style.load', () => {
         'source': 'neighborhoods',
         'layout': {},
         'paint': {
-        'line-color': '#000000',
-        'line-width': 1
+            'line-color': '#000000',
+            'line-width': 1
         }
     });
 
 });
 
 // Add markers for each borough
-function buildLayer (data, borough, map){
+function buildLayer(data, borough, map) {
     let filteredData = data.filter(results => results.neighbourhood_group_cleansed == borough);
     let dataArr = []
 
     // Create Feature data for geoJSON object
     filteredData.forEach(document => {
-        var data =  {
+        var data = {
             "type": "Feature",
             "geometry": {
                 "type": "Point",
@@ -242,35 +272,35 @@ function buildLayer (data, borough, map){
     });
 
     // Add layer to the map
-    if (!map.getLayer(borough+"-layer")) {
+    if (!map.getLayer(borough + "-layer")) {
 
         const sourceName = borough + "Source"
         map.addSource(sourceName, {
-        "type": "geojson",
-        "data": {
-            "type": "FeatureCollection",
-            "features": dataArr
-        }
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": dataArr
+            }
         });
         map.addLayer({
-        "id": borough+"-layer",
-        "type": "symbol",
-        "source": sourceName,
-        "layout": {
-            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-            "text-offset": [0, 0.6],
-            "text-anchor": "top",
-            "icon-image": "star-15",
-            "icon-allow-overlap": true,
-            "visibility": "visible"
-        },
-        "paint":{
-            "icon-opacity": ["case", ["boolean", ["feature-state", "hover"], false], 0.5, 1.0]
-        }
+            "id": borough + "-layer",
+            "type": "symbol",
+            "source": sourceName,
+            "layout": {
+                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                "text-offset": [0, 0.6],
+                "text-anchor": "top",
+                "icon-image": "star-15",
+                "icon-allow-overlap": true,
+                "visibility": "visible"
+            },
+            "paint": {
+                "icon-opacity": ["case", ["boolean", ["feature-state", "hover"], false], 0.5, 1.0]
+            }
         });
     }
     else {
-        map.setLayoutProperty(borough+"-layer", "visibility", "visible");
+        map.setLayoutProperty(borough + "-layer", "visibility", "visible");
     }
 
     // Add popups to each marker
@@ -280,12 +310,12 @@ function buildLayer (data, borough, map){
         var roomType = e.features[0].properties.room_type;
         var price = e.features[0].properties.price;
         while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
         }
         new mapboxgl.Popup().on('open', function (pop) {
         }).setLngLat(coordinates).setMaxWidth(500).setHTML(`<h4>${roomType}</h4> <hr> <h4>Price: $${price}</h4>`)
-        .addTo(map);
-  });
+            .addTo(map);
+    });
 };
 
 
@@ -298,12 +328,12 @@ function changeLayer(borough) {
 
             // Create layer for borough
             buildLayer(parsedData, borough, map);
-        
+
         });
-    };
+};
 
 
-    
+
 const boroughBoxes = {   // Boxes for each borough
     'Manhattan': [[-74.0479, 40.6829], [-73.9067, 40.8820]],
     'Brooklyn': [[-74.0419, 40.5707], [-73.8567, 40.7394]],
@@ -311,17 +341,17 @@ const boroughBoxes = {   // Boxes for each borough
     'Bronx': [[-73.9339, 40.7855], [-73.7654, 40.9153]],
     'Staten Island': [[-74.2558, 40.4960], [-74.0522, 40.6490]]
 };
-    
+
 
 // Function to change view to a different borough
 function changeBorough(borough, first) {
 
     // Select previous borough
     let previousBorough = d3.select(".prev_value");
-    
+
     // Zoom to specified borough
     map.fitBounds(boroughBoxes[borough], {
-        padding: {top: 10, bottom: 25, left: 15, right: 5}
+        padding: { top: 10, bottom: 25, left: 15, right: 5 }
     });
 
     // Create plots
@@ -330,7 +360,7 @@ function changeBorough(borough, first) {
 
     // Render the correct layer
     if (first != "First") {
-    map.setLayoutProperty(previousBorough.attr("value") + "-layer", "visibility", "none");
+        map.setLayoutProperty(previousBorough.attr("value") + "-layer", "visibility", "none");
     }
     changeLayer(borough);
 
